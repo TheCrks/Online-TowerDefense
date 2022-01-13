29ee8f62c5126ef9c43df5aee5a81574cfd9d201
TheCrks <85020724+TheCrks@users.noreply.github.com>
2022-01-13 14:16:06 +0300

    Update Network.py

--- Online-TD/Kod dosyaları/Network.py
+++ Online-TD/Kod dosyaları/Network.py
@@ -5,7 +5,7 @@ import pickle
 class network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.58.7.131"
+        self.server = "ip address"
         self.port = 5555
         self.address = (self.server, self.port)
         self.player = self.connect()
